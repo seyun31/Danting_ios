bfae48db5fdfdf44fba13135af191c3b87642deb
kes3035 <kes3035@gmail.com>
2024-10-04 17:48:54 +0900

    Add: 프로젝트 생성

---
+++ Danting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Danting
+//
+//  Created by 김은상 on 10/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DantingTests/DantingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DantingTests.swift
+//  DantingTests
+//
+//  Created by 김은상 on 10/4/24.
+//
+
+import Testing
+@testable import Danting
+
+struct DantingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
