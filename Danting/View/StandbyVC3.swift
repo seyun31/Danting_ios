9fb3c3bb9506237b2d771ff6f6ea914ddd36ef20
kes3035 <kes3035@gmail.com>
2024-10-07 15:46:11 +0900

    Feat: StandbyVC3 InfoView 띄우기

--- Danting/View/StandbyVC3.swift
+++ Danting/View/StandbyVC3.swift
@@ -131,16 +131,33 @@ final class StandbyVC3: StandbyViewController {
                                 self.thirdUserImageButton, self.fourthUserImageButton,
                                 self.fifthUserImageButton, self.sixthUserImageButton]
     
+    lazy var firstInfoView = InfoView().then { $0.isHidden = true }
+    
+    lazy var secondInfoView = InfoView().then { $0.isHidden = true }
+    
+    lazy var thirdInfoView = InfoView().then { $0.isHidden = true }
+    
+    lazy var fourthInfoView = InfoView().then { $0.isHidden = true }
+    
+    lazy var fifthInfoView = InfoView().then { $0.isHidden = true }
+    
+    lazy var sixthInfoView = InfoView().then { $0.isHidden = true }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureStandyVC3()
         self.settingActionsForImageButton()
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.settingCornerRadiusForInfoView()
+    }
+    
     @objc func userButtonTapped(_ sender: UIButton) {
         print("Dubug: userButtonTapped")
         print("Debug: userButtonTag == \(sender.tag)")
-        
+        self.presentInfoView(tag: sender.tag)
         // 준비완료시 서버로 상태 전달
     }
     
@@ -148,12 +165,34 @@ final class StandbyVC3: StandbyViewController {
 }
 
 
-extension StandbyVC3 {
+extension StandbyVC3: StandbyInformation {
 
+    func presentInfoView(tag: Int) {
+        switch tag {
+        case 1:
+            firstInfoView.isHidden.toggle()
+        case 2:
+            secondInfoView.isHidden.toggle()
+        case 3:
+            thirdInfoView.isHidden.toggle()
+        case 4:
+            fourthInfoView.isHidden.toggle()
+        case 5:
+            fifthInfoView.isHidden.toggle()
+        case 6:
+            sixthInfoView.isHidden.toggle()
+        default:
+            break
+        }
+    }
+    
     private func configureStandyVC3() {
         self.view.addSubviews(firstUserStackView, secondUserStackView,
                               thirdUserStackView, fourthUserStackView,
-                              fifthUserStackView, sixthUserStackView)
+                              fifthUserStackView, sixthUserStackView,
+                              firstInfoView, secondInfoView,
+                              thirdInfoView, fourthInfoView,
+                              fifthInfoView, sixthInfoView)
         
         self.firstUserStackView.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(42)
@@ -196,12 +235,65 @@ extension StandbyVC3 {
             $0.width.equalTo(67.35)
             $0.height.equalTo(84.42)
         }
+        
+        self.firstInfoView.snp.makeConstraints {
+            $0.leading.equalTo(self.firstUserImageButton.snp.leading)
+            $0.height.equalTo(49)
+            $0.bottom.equalTo(self.firstUserImageButton.snp.top).offset(-8)
+            $0.width.equalTo(127)
+        }
+        
+        self.secondInfoView.snp.makeConstraints {
+            $0.leading.equalTo(self.secondUserImageButton.snp.leading)
+            $0.height.equalTo(49)
+            $0.bottom.equalTo(self.secondUserImageButton.snp.top).offset(-8)
+            $0.width.equalTo(127)
+        }
+        
+        self.thirdInfoView.snp.makeConstraints {
+            $0.trailing.equalTo(self.thirdUserImageButton.snp.trailing)
+            $0.height.equalTo(49)
+            $0.bottom.equalTo(self.thirdUserImageButton.snp.top).offset(-8)
+            $0.width.equalTo(127)
+        }
+        
+        self.fourthInfoView.snp.makeConstraints {
+            $0.leading.equalTo(self.fourthUserImageButton.snp.leading)
+            $0.height.equalTo(49)
+            $0.top.equalTo(self.fourthUserNameLabel.snp.bottom).offset(8)
+            $0.width.equalTo(127)
+        }
+        
+        self.fifthInfoView.snp.makeConstraints {
+            $0.trailing.equalTo(self.fifthUserImageButton.snp.trailing)
+            $0.height.equalTo(49)
+            $0.top.equalTo(self.fifthUserNameLabel.snp.bottom).offset(8)
+            $0.width.equalTo(127)
+        }
+        
+        self.sixthInfoView.snp.makeConstraints {
+            $0.trailing.equalTo(self.sixthUserImageButton.snp.trailing)
+            $0.height.equalTo(49)
+            $0.top.equalTo(self.sixthUserNameLabel.snp.bottom).offset(8)
+            $0.width.equalTo(127)
+        }
+        
+        
+        
     }
     private func settingActionsForImageButton() {
         self.buttons.forEach {
-            
             $0.addTarget(self, action: #selector(userButtonTapped(_:)), for: .touchUpInside)
         }
+    }
+    
+    private func settingCornerRadiusForInfoView() {
+        self.firstInfoView.roundCorners(topLeft: 25.5, topRight: 25, bottomRight: 25)
+        self.secondInfoView.roundCorners(topLeft: 25.5, topRight: 25, bottomRight: 25)
+        self.thirdInfoView.roundCorners(topLeft: 25, topRight: 25.5, bottomLeft: 25)
         
+        self.fourthInfoView.roundCorners(topRight: 25, bottomLeft: 25.5 ,bottomRight: 25)
+        self.fifthInfoView.roundCorners(topLeft: 25, bottomLeft: 25, bottomRight: 25.5)
+        self.sixthInfoView.roundCorners(topLeft: 25, bottomLeft: 25 ,bottomRight: 25.5)
     }
 }
